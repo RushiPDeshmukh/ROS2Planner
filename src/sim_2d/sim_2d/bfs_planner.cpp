a1b87b8b0ca42739c93b88bdd039004b15e70dbc
rushipdeshmukh <rdeshmukh@wpi.edu>
2022-12-22 15:28:38 -0500

    removed c++ files

--- src/sim_2d/sim_2d/bfs_planner.cpp
+++
@@ -1,2 +0,0 @@
-// This code will take the adj_matrix from csv
-// This code will run a service where it will take the start and goal and return path vector
--- src/sim_2d/sim_2d/maze_generator.cpp
+++
@@ -1,3 +0,0 @@
-// This program will create the maze 
-// Will run a service that provides a adj_list when asked
-
