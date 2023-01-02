76e09b3d47070106ebdb2e0e7411d36c06812113
rushipdeshmukh <rdeshmukh@wpi.edu>
2023-01-02 14:37:28 -0500

    Updated Simulator code

--- src/wildfire_project/wildfire_project/wildfire_simulator.py
+++ src/wildfire_project/wildfire_project/wildfire_simulator.py
@@ -15,9 +15,6 @@
 """
 import numpy as np
 import pygame
-from math import *
-import os
-import time
 
 #TO-DO
 # 1. define a random patch() function this function defines a obstacle shape as numpy array
@@ -33,52 +30,59 @@ class Bush:
         self.x = pos[0]
         self.y = pos[1]
         self.tree_width = tree_width #this variable sets the width of single unit of the patch.
-        self.data,self.shape = random_patch() #this function returns a random forest shape and its shape variable
+        self.shape = self.random_patch() #this function returns a random forest shape and its shape variable
         self.on_fire = False
 
     def get_data(self):
-        return self.pos,self.data,self.shape
+        return self.pos,self.shape
 
     def change_shape(self):
-        self.data,self.shape = random_patch()
-        
-        return self.data,self.shape
+        self.shape = self.random_patch()
+        return self.shape
 
     def set_fire(self):
         self.on_fire = True
         
-
     def extinguish_fire(self):
         self.on_fire = False
 
+    def get_rect(self):
+        return pygame.Rect(self.x,self.y,self.shape[0]*self.tree_width,self.shape[1]*self.tree_width)
+
     def get_boundary(self):
         x,y = self.pos
-        center_x,center_y = ((self.shape[0]*self.tree_width + x)//2,(self.shape[1]*self.tree_width + y)//2)
-        radius = ((center_x-x)**2 + (center_y+y)**2)**0.5
-        center = center_x,center_y
+        center = ((self.shape[0]*self.tree_width + x)//2,(self.shape[1]*self.tree_width + y)//2)
+        radius = ((center[0]-x)**2 + (center[1]+y)**2)**0.5
         return (center,radius)
 
-    def draw(self,win):
+    def draw(self,surface):
         x,y = self.pos
-        # rect = self.nearby_bush()
-        # pygame.draw.rect(win,BLUE,rect,width = 2)
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
-                if self.data[i][j] == 1:
-                    win.blit(self.surface,(x+(self.tree_width*i),y+(self.tree_width*j)))
+                color = ((np.random.rand()*10+10)*(1+self.on_fire*10),(np.random.rand()*10+5)*(1+(not self.on_fire)*10),0)
+                rect = (x+i*self.tree_width,y+j*self.tree_width,self.tree_width,self.tree_width)
+                pygame.draw.rect(surface,color,rect,0,3)
+    
+    def random_patch(self):
+        x = np.random.randint(0,4)
+        y = np.random.randint(0,4)
+        return (x,y)
 
 
 class Forest:
-    def __init__(self,coverage = 20,width = 500,bush_width = 10) -> None:
+    def __init__(self,coverage = 10,width = 500,bush_width = 10) -> None:
         self.coverage = coverage
         self.width = width
         self.bush_width = bush_width
         self.number_of_bushes = 0
         self.bushes = []
-        self.grid_data = np.zeros((self.width//(self.bush_width),self.width//(self.bush_width)))
+        
+        # self.grid_data = np.zeros((self.width//(self.bush_width),self.width//(self.bush_width))) CHANGE
     
     def can_extinguish(self,car_boundary):
         pass
+
+
 
     def create(self):
         """This function will randomly generate the Bush objects to populate the forest
@@ -89,33 +93,37 @@ class Forest:
         total_area = (self.width**2) #total area of forest
         coverage = occupied*100//total_area #current coverage of forest
         while coverage<self.coverage:
+            print("Current Coverage: ",coverage)
             #random pos in the forest
-            random_x = np.random.randint(0,self.width//(self.bush_width*3))*(self.bush_width*3)
-            random_y = np.random.randint(0,self.width//(self.bush_width*3))*(self.bush_width*3)
+            random_x = np.random.randint(0,self.width)
+            random_y = np.random.randint(0,self.width)
             #generate the bush object at this location
             bush = Bush((random_x,random_y),self.bush_width)
-            _,bush_data,shape = bush.get_data()
-            counter = 7
-            while not ((random_x+shape[0]*self.bush_width < self.width) and (random_y+shape[1]*self.bush_width < self.width)) and counter != 0:
-                bush_data,shape = bush.change_shape()
+            _,shape = bush.get_data()
+            counter = 3
+            while not ((random_x+shape[0]*self.bush_width < self.width) and (random_y+shape[1]*self.bush_width < self.width)):
+                shape = bush.change_shape()
                 counter -=1
-            
-            if not ((random_x+shape[0]*self.bush_width < self.width) and (random_y+shape[1]*self.bush_width < self.width)):
-                continue
+                if counter <0:
+                    continue
 
-            rect_list = bush.get_rects()
+            rect = bush.get_rect()
             #check if the bush fits inside the forest and add to the list of bushes 
-            if not self.is_collision(rect_list):
+            if not self.is_collision(rect):
                 self.bushes.append(bush)
+                print("pos: ",bush.pos)
+                print("shape:",bush.shape )
                 self.number_of_bushes +=1
-                occupied += bush_data.sum()*self.bush_width**2
-                for i in range(shape[0]):
-                    for j in range(shape[1]):
-                        x = i + random_x//(self.bush_width)
-                        y = j + random_y//(self.bush_width)
-                        self.grid_data[x][y] = bush_data[i][j]
+                occupied += shape[0]*shape[1]*self.bush_width**2
             coverage = occupied*100//total_area
-        
+
+    def is_collision(self,rect2):
+        for bush in self.bushes:
+            rect1 = bush.get_rect()
+            if rect1.colliderect(rect2):
+                return True
+        return False
+
     def get_grid(self):
         return self.grid_data
     
@@ -129,47 +137,33 @@ class Forest:
         pass
 
     
-    def draw(self,win):
+    def draw(self,surface):
         for bush in self.bushes:
-            bush.draw(win)
+            bush.draw(surface)
 
 
 if __name__ == "__main__":
-    width_win = 500
     
-    win = pygame.display.set_mode((width_win,width_win))
-    pygame.display.set_caption("Title")
-    pygame.font.init()
-    font = pygame.font.Font('freesansbold.ttf',10)
-    text = font.render("T",True,BLACK)
-    textrect = text.get_rect()
-    textrect.topleft = (450,10)
+    BLACK = (0,0,0)
+    WHITE = (255,255,255)
+    win = pygame.display.set_mode((1000,700))
+    surf1 = pygame.surface.Surface((500,500))
     
     run = True
     pos = (0,0)
     f = Forest()
     f.create()
-    t_last = time.time()
-    t = 0           
-    while t <= 300 and run:
+   
+    while run:
         win.fill(WHITE)
+        surf1.fill(WHITE)
+        pygame.draw.rect(surf1,BLACK,(0,0,500,500),3,8)
         events = pygame.event.get()
         for ev in events:
             if ev.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-        if t%30 == 0:
-            f.trigger_fire(t)
-        #Spreading fire is too quick
-        if t%20 == 0:
-             f.spread_fire(t)
-        f.draw(win)
-        text = font.render("Time: "+str(t),True,BLACK)
-        win.blit(text,textrect) 
+        f.draw(surf1)
+        win.blit(surf1,(240,100))
         pygame.display.update()
         pygame.time.wait(1000)
-        t += int(time.time() - t_last)
-        t_last = time.time()
-        print(t)
-
-
